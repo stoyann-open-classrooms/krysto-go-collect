d58e716532212475ff100e2218c7eefd89b12390
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2023-02-20 00:23:36 +1100

    qr generator

--- src/pages/private/PrivateCollectDetails.jsx
+++ src/pages/private/PrivateCollectDetails.jsx
@@ -4,6 +4,7 @@ import { getCollect, reset } from "../../features/collect/collectSlice";
 import { useParams } from "react-router-dom";
 import Spinner from "../../components/spinner/Spinner";
 import { toast } from "react-toastify";
+import QRCode from "qrcode.react";
 
 function PrivateCollectDetails() {
   const { collect, isLoading, isSuccess, isError, message } = useSelector(
@@ -11,8 +12,6 @@ function PrivateCollectDetails() {
   );
   const params = useParams();
   const dispatch = useDispatch();
-
- 
 
   useEffect(() => {
     if (isError) {
@@ -23,36 +22,57 @@ function PrivateCollectDetails() {
   }, [isError, message, params.id]);
 
   console.log(collect);
-if(!collect.data || isLoading) {
-    return <Spinner/>
-}
-if(isError) {
-    return <h3>Une erreur est survenue merci de réesayer.</h3>
-}
-  return <div className="ticket-page"> 
-  
-    <header className="ticket-header">
+  const qrData = [
+ collect.data._id,
+   collect.data.collectPoint.waste,
+  collect.data.collectPoint._id,
+
+     ]
+
+  if (!collect.data || isLoading) {
+    return <Spinner />;
+  }
+  if (isError) {
+    return <h3>Une erreur est survenue merci de réesayer.</h3>;
+  }
+  return (
+    <div className="ticket-page">
+      <header className="ticket-header">
         <button className="btn btn-sm">Retour</button>
-        <h2>Client : {collect.data.collectPoint.partnerName} <span className="status">{collect.data.collectPoint.location.city}</span></h2>
-        <h2>ID de la collecte : {collect.data._id} <span className={`status collect-${collect.data.status}` }>{collect.data.status}</span></h2>
-        <h4>Date de la demande : {new Date(collect.data.collectDemandeDate).toLocaleDateString()}</h4>
+       
+        <h2>
+          Client : {collect.data.collectPoint.partnerName}{" "} <br />
+          Date de la demande :{" "} 
+          
+          {new Date(collect.data.collectDemandeDate).toLocaleDateString()}
+          <span>
+          <div>
+          <QRCode value={qrData} />
+        </div>
+          </span>
+        </h2>
+        <hr />
+        
+        <h2>
+         status :
+          <span className={`status collect-${collect.data.status}`}>
+            {collect.data.status}
+          </span>
+        </h2>
+        
         <hr />
         <div className="ticket-desc">
-            <h3>Remarque: </h3>
-        <p>{collect.data.collectPoint.description}</p>
-        
+          <h3>Remarque: </h3>
+          <p>{collect.data.collectPoint.description}</p>
         </div>
         <div className="ticket-desc">
-            <h3>Adresse: </h3>
-        <p>{collect.data.collectPoint.location.formattedAddress}</p>
+          <h3>Adresse: </h3>
+          <p>{collect.data.collectPoint.location.formattedAddress}</p>
         </div>
-    </header>
-    <section>
-
-
-
-    </section>
-   </div>;
+      </header>
+      <section></section>
+    </div>
+  );
 }
 
 export default PrivateCollectDetails;
